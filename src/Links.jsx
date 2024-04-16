7917c9760587f07c36cc8ebb85ad6c735e184ceb
BrennanTibbetts <brennanttibbetts@gmail.com>
2024-04-16 18:57:27 -0400

    Boxle Link

--- src/Links.jsx
+++ src/Links.jsx
@@ -2,7 +2,11 @@
 export default function Links()
 {
     return <>
+        <a className="boxle" href="https://boxle.vercel.app" target="_blank">
+            Check Out My New Game: Boxle (In Dev)
+        </a>
         <div id="links">
+
             <a href="https://github.com/BrennanTibbetts" target="_blank">
                 <img src="/css/github.png"/>
             </a>
